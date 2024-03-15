54add8e2ef444cceaac17ab72050fe149711854e
iagob00 <iago.barretto1@hotmail.com>
2024-03-14 21:12:35 -0300

    test
    testeee

--- app/src/main/java/com/example/homeowner_companion/ExpensesActivity.kt
+++ app/src/main/java/com/example/homeowner_companion/ExpensesActivity.kt
@@ -7,5 +7,7 @@ class ExpensesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expenses)
+
+        //aaaaaaa
     }
 }
